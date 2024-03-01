75498898a9b38ca0b8ff5afcfd59311065753395
AlexByte <alex.g.cortes.g@gmail.com>
2024-02-29 23:05:22 -0600

    feat: category crud

--- app/Modules/Category/Category.php
+++ app/Modules/Category/Category.php
@@ -4,10 +4,16 @@ namespace Modules\Category;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Str;
 class Category extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::of($value)->slug('-');
+    }
 }
--- app/Modules/Category/CategoryController.php
+++ app/Modules/Category/CategoryController.php
@@ -4,7 +4,9 @@ namespace Modules\Category;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Modules\Category\Requests\StoreCategoryRequest;
 use Modules\Category\Resources\CategoryCollection;
+use Modules\Category\Resources\CategoryResource;
 
 class CategoryController extends Controller
 {
@@ -18,5 +20,42 @@ class CategoryController extends Controller
         $categories = Category::paginate();
 
         return new CategoryCollection($categories);
+    }
+
+    public function show(Category $category)
+    {
+        return new CategoryResource($category);
+    }
+
+    public function store(StoreCategoryRequest $request)
+    {
+        $data = $request->validated();
+
+        $category = Category::create([
+            'name' => $data['name'],
+            'description' => $data['description'],
+            'user_id' => auth()->user()->id,
+        ]);
+
+        return new CategoryResource($category);
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return response()->noContent();
+    }
+
+    public function update(Category $category, StoreCategoryRequest $request)
+    {
+        $data = $request->validated();
+
+        $category->update([
+            'name' => $data['name'],
+            'description' => $data['description']
+        ]);
+
+        return new CategoryResource($category);
     }
 }
